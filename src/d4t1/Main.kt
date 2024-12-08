6e4ecff8f53e8225bc45cd24b70378a5b2ad0fb0
Nikita Yakunin <nayakunin99@gmail.com>
2024-12-08 11:36:40 +0100

    day4 complete

--- src/d4t1/Main.kt
+++ src/d4t1/Main.kt
@@ -21,77 +21,125 @@ fun main() {
         matrix2.add(MutableList(line.length) { '.' })
     }
 
-    fun pick(x: Int, y: Int, direction: Direction): Pair<String?, List<Pair<Int, Int>>> {
+    fun pickString(x: Int, y: Int, direction: Direction): String? {
+        var result: List<Char>? = null
+
         when (direction) {
             Direction.UpRight -> {
                 if (y >= SIZE - 1 && x <= matrix[y].size - SIZE) {
-                    return Pair(
-                        listOf(matrix[y][x], matrix[y - 1][x + 1], matrix[y - 2][x + 2], matrix[y - 3][x + 3]).joinToString(""),
-                        listOf(
-                            Pair(x, y),
-                            Pair(x + 1, y - 1),
-                            Pair(x + 2, y - 2),
-                            Pair(x + 3, y - 3),
-                        )
+                    result = listOf(
+                        matrix[y][x],
+                        matrix[y - 1][x + 1],
+                        matrix[y - 2][x + 2],
+                        matrix[y - 3][x + 3]
                     )
                 }
             }
+
             Direction.Right -> {
                 if (x <= matrix[y].size - SIZE) {
-                    return Pair(
-                        listOf(matrix[y][x], matrix[y][x + 1], matrix[y][x + 2], matrix[y][x + 3]).joinToString(""),
-                        listOf(
-                            Pair(x, y),
-                            Pair(x + 1, y),
-                            Pair(x + 2, y),
-                            Pair(x + 3, y),
-                        )
+                    result = listOf(
+                        matrix[y][x],
+                        matrix[y][x + 1],
+                        matrix[y][x + 2],
+                        matrix[y][x + 3]
                     )
                 }
             }
+
             Direction.DownRight -> {
                 if (y <= matrix.size - SIZE && x <= matrix[y].size - SIZE) {
-                    return Pair(
-                        listOf(matrix[y][x], matrix[y + 1][x + 1], matrix[y + 2][x + 2], matrix[y + 3][x + 3]).joinToString(""),
-                        listOf(
-                            Pair(x, y),
-                            Pair(x + 1, y + 1),
-                            Pair(x + 2, y + 2),
-                            Pair(x + 3, y + 3),
-                        )
+                    result = listOf(
+                        matrix[y][x],
+                        matrix[y + 1][x + 1],
+                        matrix[y + 2][x + 2],
+                        matrix[y + 3][x + 3]
                     )
                 }
             }
+
             Direction.Down -> {
                 if (y <= matrix.size - SIZE) {
-                    return Pair(
-                        listOf(matrix[y][x], matrix[y + 1][x], matrix[y + 2][x], matrix[y + 3][x]).joinToString(""),
-                        listOf(
-                            Pair(x, y),
-                            Pair(x, y + 1),
-                            Pair(x, y + 2),
-                            Pair(x, y + 3),
-                        )
+                    result = listOf(
+                        matrix[y][x],
+                        matrix[y + 1][x],
+                        matrix[y + 2][x],
+                        matrix[y + 3][x]
                     )
                 }
             }
         }
 
-        return Pair(null, listOf())
+        return result?.joinToString("")
+    }
+
+    fun pickIndexes(x: Int, y: Int, direction: Direction): List<Pair<Int, Int>>? {
+        var result: List<Pair<Int, Int>>? = null
+
+        when (direction) {
+            Direction.UpRight -> {
+                if (y >= SIZE - 1 && x <= matrix[y].size - SIZE) {
+                    result = listOf(
+                        Pair(x, y),
+                        Pair(x + 1, y - 1),
+                        Pair(x + 2, y - 2),
+                        Pair(x + 3, y - 3),
+                    )
+                }
+            }
+
+            Direction.Right -> {
+                if (x <= matrix[y].size - SIZE) {
+                    result = listOf(
+                        Pair(x, y),
+                        Pair(x + 1, y),
+                        Pair(x + 2, y),
+                        Pair(x + 3, y),
+                    )
+                }
+            }
+
+            Direction.DownRight -> {
+                if (y <= matrix.size - SIZE && x <= matrix[y].size - SIZE) {
+                    result = listOf(
+                        Pair(x, y),
+                        Pair(x + 1, y + 1),
+                        Pair(x + 2, y + 2),
+                        Pair(x + 3, y + 3),
+                    )
+                }
+            }
+
+            Direction.Down -> {
+                if (y <= matrix.size - SIZE) {
+                    result = listOf(
+                        Pair(x, y),
+                        Pair(x, y + 1),
+                        Pair(x, y + 2),
+                        Pair(x, y + 3),
+                    )
+                }
+            }
+        }
+
+        return result
     }
 
     var count = 0
 
-    for (y in 0..<matrix.size) {
-        for (x in 0..<matrix[y].size) {
+    for (y in 0 ..< matrix.size) {
+        for (x in 0 ..< matrix[y].size) {
             if (matrix[y][x] != 'X' && matrix[y][x] != 'S') {
                 continue
             }
             for (direction in Direction.entries) {
-                val (result, idxs) = pick(x, y, direction)
+                val result = pickString(x, y, direction)
                 if (result == "XMAS" || result == "XMAS".reversed()) {
-                    for ((i, j) in idxs) {
-                        matrix2[j][i] = matrix[j][i]
+                    val idxs = pickIndexes(x, y, direction)
+                    if (idxs != null) {
+                        for ((i, j) in idxs) {
+                            matrix2[j][i] = matrix[j][i]
+                        }
                     }
                     count += 1
                 }
